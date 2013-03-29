bd945d8e9382551fff3c94933a84aab1fc17ef08
Mattias Johansson <mattias.johansson@atex.com>
2013-03-29 19:43:56 +0100

    Added background music to the woods scene

--- src/com/folkol/paskhack/Woods.java
+++ src/com/folkol/paskhack/Woods.java
@@ -2,16 +2,20 @@ package com.folkol.paskhack;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class Woods extends Scene {
     private Hero hero;
     private int screenPosX, screenPosY;
+    private Music music;
 
     public Woods() throws SlickException {
         map = new TiledMap("/maps/woods.tmx");
         hero = new Hero();
+        music = new Music("/snd/woods.ogg");
+        music.loop(1.0f, 0.2f);
     }
 
     @Override
