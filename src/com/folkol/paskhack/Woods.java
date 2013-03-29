f15e73057453738fd53c5d8b29a5c8c9f1834809
Mattias Johansson <mattias.johansson@atex.com>
2013-03-30 00:38:39 +0100

    Can now read monsters from the map, and also kill them

--- src/com/folkol/paskhack/Woods.java
+++ src/com/folkol/paskhack/Woods.java
@@ -16,7 +16,19 @@ public class Woods extends Scene {
         map = new TiledMap("/maps/woods.tmx");
         hero = new Hero(this);
         entities.add(hero);
-        entities.add(new Monster(this));
+        for (int i = 0; i < map.getObjectCount(0); i++) {
+            String objectType = map.getObjectType(0, i);
+            int objectX = map.getObjectX(0, i);
+            int objectY = map.getObjectY(0, i);
+            if (objectType.equals("monster")) {
+                Monster monster = new Monster(this);
+                monster.setX(objectX);
+                monster.setY(objectY);
+                entities.add(monster);
+            } else {
+                System.out.println("Unknown object type found");
+            }
+        }
         music = new Music("/snd/woods.ogg");
         music.loop(1.0f, 0.1f);
     }
@@ -24,11 +36,9 @@ public class Woods extends Scene {
     @Override
     public void update(GameContainer gc, int delta) {
         hero.update(gc, delta);
-
-        for(Entity e : entities) {
+        for (Entity e : entities) {
             e.update(gc, delta);
         }
-
         screenPosX = (int) (hero.getX() - 400);
         screenPosY = (int) (hero.getY() - 300);
         if (screenPosX < 0) {
@@ -37,14 +47,11 @@ public class Woods extends Scene {
         if (screenPosY < 0) {
             screenPosY = 0;
         }
-
         Collections.sort(entities, new Comparator<Entity>() {
-
             @Override
             public int compare(Entity e1, Entity e2) {
                 return (int) (e1.y - e2.y);
             }
         });
     }
-
 }
