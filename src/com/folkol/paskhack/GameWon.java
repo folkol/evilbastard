e964632a3b8bef9642b2f57e0a4a0c995e530eef
Mattias Johansson <mattias.johansson@atex.com>
2013-03-30 04:21:18 +0100

    Added credentials

--- src/com/folkol/paskhack/GameWon.java
+++ src/com/folkol/paskhack/GameWon.java
@@ -3,6 +3,7 @@ package com.folkol.paskhack;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -21,12 +22,60 @@ public class GameWon extends Scene {
         music = new Music("/snd/woods.ogg");
         image = new Image("/gfx/outro.png");
         nextScene = this;
-        text.add("Lorem Ipsum");
+        text.add("Congratulations!");
+        text.add("");
+        text.add("");
+        text.add("You have finally killed the evil bastard.");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+
+        text.add("...");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("There are always more evil bastards to kill!");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("Programmer: Folkol");
+        text.add("");
+        text.add("");
+        text.add("Sound artist: Folkol");
+        text.add("");
+        text.add("");
+        text.add("Graphics artist: Folkol");
+        text.add("");
+        text.add("");
+        text.add("Game designer: Folkol");
+        text.add("");
+        text.add("");
+        text.add("Game designer: Folkol");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("");
+        text.add("Glad påsk! :-)");
+
         reset();
     }
 
     public void reset() throws SlickException {
-        x = 500;
+        x = 400;
         y = 850;
         finished = false;
         music.loop(1.0f, 0.1f);
@@ -34,7 +83,7 @@ public class GameWon extends Scene {
 
     @Override
     public void update(GameContainer gc, int delta) {
-        y -= 0.1 * delta;
+        y -= 0.05 * delta;
         if (gc.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
             gc.exit();
         }
@@ -43,6 +92,7 @@ public class GameWon extends Scene {
     @Override
     public void render(GameContainer gc, Graphics g) {
         image.draw();
+        g.setColor(Color.white);
         for(int i = 0; i < text.size(); i++) {
             g.drawString(text.get(i), x, y + i * 20);
         }
